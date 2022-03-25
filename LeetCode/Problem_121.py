b0d8da4b43b3dd2af2b8c521f99666ebf2e3b448
MichaelBenliyan <michaelbenliyan@gmail.com>
2022-03-25 15:36:50 -0700

    LeetCode

---
+++ LeetCode/Problem_121.py
@@ -0,0 +1,15 @@
+def maxProfit(self, prices):
+    """
+    :type prices: List[int]
+    :rtype: int
+    """
+    buy = 0
+    sell = 1
+    max_profit = 0
+    while sell < len(prices):
+        if prices[sell] - prices[buy] > max_profit:
+            max_profit = prices[sell] - prices[buy]
+        if prices[buy] > prices[sell]: 
+            buy = sell   
+        sell += 1
+    return max_profit
